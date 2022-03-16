abd9527eb75a79a5b19140352d22a1ec19c0276d
githvb495 <101061829+githvb495@users.noreply.github.com>
2022-03-16 15:15:11 +0000

    Connection website-database

--- php/db_conn_web_conf.php
+++ php/db_conn_web_conf.php
@@ -9,6 +9,7 @@ $db_name = 'phpdb';
     $var = $_SERVER['PHP_SELF'];    -- La página del servidor a la que tiene que llamar 
                                     -- ex. El formulario y los resultados del formulario se encuentran en la misma página.
     mysqli_set_charset( $conn, 'utf8' );
+    mysqli_affected_rows($conn);    -- Devuelve (int) el número de filas afectadas por INSERT, UPDATE, DELETE
 */
 
 ?>
