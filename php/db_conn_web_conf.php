ebcccd9aa0fbe78e434d441c08fe23a4fc7d92cf
githvb495 <dev495@pm.me>
2022-03-16 22:51:23 +0000

    Connection website-database

--- php/db_conn_web_conf.php
+++ php/db_conn_web_conf.php
@@ -5,13 +5,4 @@ $db_user = 'root';
 $db_pass = '';
 $db_name = 'phpdb';
 
-/*  Util
-    $var = $_SERVER['PHP_SELF'];    -- La página del servidor a la que tiene que llamar 
-                                    -- ex. El formulario y los resultados del formulario se encuentran en la misma página.
-    mysqli_set_charset( $conn, 'utf8' );
-    mysqli_affected_rows($conn);    -- Devuelve (int) el número de filas afectadas por INSERT, UPDATE, DELETE
-    mysqli_num_rows()               -- Devuelve el número de registros en una consulta SELECT
-    mysqli_query($conn, $query)     -- boolean : true si se ejecutó la consulta
-*/
-
 ?>
