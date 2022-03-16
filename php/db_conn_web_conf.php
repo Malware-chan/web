951282c0c48b2139180d52071d103f9a2739dacf
githvb495 <dev495@pm.me>
2022-03-16 16:59:04 +0000

    Connection website-database

--- php/db_conn_web_conf.php
+++ php/db_conn_web_conf.php
@@ -10,6 +10,8 @@ $db_name = 'phpdb';
                                     -- ex. El formulario y los resultados del formulario se encuentran en la misma página.
     mysqli_set_charset( $conn, 'utf8' );
     mysqli_affected_rows($conn);    -- Devuelve (int) el número de filas afectadas por INSERT, UPDATE, DELETE
+    mysqli_num_rows()               -- Devuelve el número de registros en una consulta SELECT
+    mysqli_query($conn, $query)     -- boolean : true si se ejecutó la consulta
 */
 
 ?>
