6bd199fb845a0ce9586e6df9988a155436b0a1a3
githvb495 <dev495@pm.me>
2022-03-16 17:53:26 +0000

    Connection website-database

--- php/db_conn_web.php
+++ php/db_conn_web.php
@@ -4,7 +4,7 @@ ini_set( 'display_errors', 1 );
 error_reporting( ~0 );
 
 if ( isset( $_POST[ 'button' ] ) ) {
-    require('db_conn_web_conf.php');
+    require( 'db_conn_web_conf.php' );
 
     /* Conexión por procedimientos */
     $connp = mysqli_connect( $db_host, $db_user, $db_pass );
@@ -24,12 +24,33 @@ if ( isset( $_POST[ 'button' ] ) ) {
     }
 
     while ( $row = mysqli_fetch_array( $result_set, MYSQLI_ASSOC ) ) {
-        echo $row['username'];
-        echo $row['password'];
-        echo $row['email'];
+        echo $row[ 'username' ];
+        echo $row[ 'password' ];
+        echo $row[ 'email' ];
     }
 
-    mysqli_close($connp);
+    mysqli_close( $connp );
+
+    /* Conexión por objeto MySQLi */
+    $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+    if ($conn -> connect_errno) {
+        echo $conn -> connect_errno;
+    }
+
+    $conn -> set_charset('utf8');
+    $query = 'SELECT * FROM users';
+    $result_set = $conn -> query($query);
+    if ($conn -> errno) {
+        die($conn -> errno);
+    }
+
+    while ($row = $result_set -> fetch_assoc()) {
+        echo $row[ 'username' ];
+        echo $row[ 'password' ];
+        echo $row[ 'email' ];
+    }
+
+    $conn -> close();
 }
 
 ?>
