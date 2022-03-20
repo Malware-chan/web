9f743d987a71eff46e0086bedb7fcaf1658ac64e
githvb495 <dev495@pm.me>
2022-03-20 14:57:04 +0000

    .

---
+++ js/tooling/tooling.js
@@ -0,0 +1,9 @@
+/* For web browsers only */
+
+/* Transpiler :: translator + compiler */
+// ES6 -> Transpiler > ES5
+// ex. 'Babel'
+
+/* Bundler */
+// Turn multiple JS files into a single one
+// ex. 'Webpack'
